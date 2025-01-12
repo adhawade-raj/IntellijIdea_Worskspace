99cdf2173501aff088b61a5740f2b289fd1a1076
adhawade-raj <adhawaderaj@gmail.com>
2025-01-12 13:47:54 +0530

    Project level modifications

--- bddCucumber_2025/src/test/java/testRunner/amazonTest.java
+++ bddCucumber_2025/src/test/java/testRunner/amazonTest.java
@@ -5,11 +5,11 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/resources/Search.feature",
+@CucumberOptions(features = "src/test/resources/Search.feature",
         glue = "org.stepDefinations",
         plugin = {
         "pretty" }, // Ensures that the output is printed to the console
         strict = true)
 
-public class amazonTestRunner {
+public class amazonTest {
 }
